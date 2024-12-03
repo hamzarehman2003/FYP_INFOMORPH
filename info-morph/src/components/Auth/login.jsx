7af0e9a55d3257340f951f20019701eb477d1948
Tallal <mmtallal@gmail.com>
2024-12-03 23:09:03 +0500

    Login Button

--- info-morph/src/components/Auth/login.jsx
+++ info-morph/src/components/Auth/login.jsx
@@ -1,36 +1,43 @@
 "use client";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Heading from "./heading";
 import InputField from "./inputfield";
 import AuthWrapper from "./wrapper";
 import WithOthers from "./withOthers";
 import WithGoogle from "./withGoogle";
 import BlueButton from "./blueButton";
+
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    router.push("/url-link");
+  };
+
   return (
     <AuthWrapper>
-      <Heading heading={"Continue on"} className={"mb-10"} />
-      <div className="w-[364px] flex flex-col gap-7">
-        <InputField
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          name={"email"}
-          placeholder={"Email"}
-          type={"text"}
-        />
-        <InputField
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          name={"password"}
-          placeholder={"Password"}
-          type={"password"}
-        />
-      </div>
-      <WithOthers name={"Log in"} />
-      <WithGoogle name={"Log in"} />
-      <BlueButton text={"Login"} />
+      <Heading heading="Continue on" className="mb-10" />
+      <form onSubmit={handleLogin}>
+        <div className="w-[364px] flex flex-col gap-7">
+          <InputField
+            name="email"
+            placeholder="Email"
+            type="text"
+          />
+          <InputField
+            name="password"
+            placeholder="Password"
+            type="password"
+          />
+        </div>
+        <WithOthers name="Log in" />
+        <WithGoogle name="Log in" />
+        <div className="flex justify-center">
+        <BlueButton text="Login" type="submit" />
+        </div>
+        
+      </form>
     </AuthWrapper>
   );
 };
